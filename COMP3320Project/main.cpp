2919b318018811eff01616e882a5940f4c5fa29b
jackratcliffe <jack-rat@hotmail.com>
2017-09-15 00:39:19 +1000

    added model loading but get error

--- COMP3320Project/main.cpp
+++ COMP3320Project/main.cpp
@@ -10,8 +10,12 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+
 #include "Shader.h"
 #include "Camera.h"
+
+#include "Model.h"
 
 const GLint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
@@ -29,6 +33,8 @@ bool firstMouse = true;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
+
+Assimp::Importer importer;
 
 int main() {
 	glfwInit();
@@ -120,16 +126,29 @@ int main() {
 
 	glm::vec3 cubePositions[] = {
 		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(2.0f, 5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f, 3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f, 2.0f, -2.5f),
-		glm::vec3(1.5f, 0.2f, -1.5f),
-		glm::vec3(-1.3f, 1.0f, -1.5f),
+		glm::vec3(2.0f, 0.0f, -15.0f),
+		glm::vec3(-1.5f, 0.0f, -2.5f),
+		glm::vec3(-3.8f, 0.0f, -12.3f),
+		glm::vec3(2.4f, 0.0f, -3.5f),
+		glm::vec3(-1.7f, 0.0f, -7.5f),
+		glm::vec3(1.3f, 0.0f, -2.5f),
+		glm::vec3(1.5f, 0.0f, -2.5f),
+		glm::vec3(1.5f, 0.0f, -1.5f),
+		glm::vec3(-1.3f, 0.0f, -1.5f),
+		glm::vec3(0.0f, -1.0f, 0.0f),
 	};
+	//glm::vec3 cubePositions[] = {
+	//	glm::vec3(0.0f, 0.0f, 0.0f),
+	//	glm::vec3(2.0f, 5.0f, -15.0f),
+	//	glm::vec3(-1.5f, -2.2f, -2.5f),
+	//	glm::vec3(-3.8f, -2.0f, -12.3f),
+	//	glm::vec3(2.4f, -0.4f, -3.5f),
+	//	glm::vec3(-1.7f, 3.0f, -7.5f),
+	//	glm::vec3(1.3f, -2.0f, -2.5f),
+	//	glm::vec3(1.5f, 2.0f, -2.5f),
+	//	glm::vec3(1.5f, 0.2f, -1.5f),
+	//	glm::vec3(-1.3f, 1.0f, -1.5f),
+	//};
 	
 	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -151,25 +170,33 @@ int main() {
 
 	glBindVertexArray(0);
 
-	GLuint texture;
+
+	GLuint textures[2];
+	glGenTextures(2, textures);
 
 	int width, height;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	unsigned char* image;
 
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	unsigned char *image = SOIL_load_image("images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
+	image = SOIL_load_image("images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glUniform1i(glGetUniformLocation(ourShader.Program, "texture0"), 0);
 
 
+	Shader shader("shaders/modelLoading.vs", "shaders/core.frag");
+
+	Model ourModel("models/Carriage.obj");
+
+	glm::mat4 projection = glm::perspective(camera.getZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 1000.0f);
 
 	//Game Loop
 	while (!glfwWindowShouldClose(window)) {
@@ -187,16 +214,32 @@ int main() {
 		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture"), 0);
+		shader.Use();
+
+		glm::mat4 view = camera.GetViewMatrix();
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+		// Draw the loaded model
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.005f, 0.005f, 0.005f));	// It's a bit too big for our scene, so scale it down
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		ourModel.Draw(shader);
+
+
+
+
+		//glActiveTexture(GL_TEXTURE1);
+		//glBindTexture(GL_TEXTURE_2D, textures[1]);
+		//glUniform1i(glGetUniformLocation(ourShader.Program, "texture1"), 1);
 
 		ourShader.Use();
 
-		glm::mat4 projection = glm::perspective(camera.getZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 1000.0f);
+		//glm::mat4 projection = glm::perspective(camera.getZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 1000.0f);
 
-		glm::mat4 model;
-		glm::mat4 view;
+		//glm::mat4 model;
+		//glm::mat4 view;
 		view = camera.GetViewMatrix();
 
 		GLint modelLoc = glGetUniformLocation(ourShader.Program,"model");
@@ -212,11 +255,17 @@ int main() {
 			glm::mat4 model;
 			model = glm::translate(model, cubePositions[i]);
 			GLfloat angle = 20.0f * i;
-			model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+			//model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+			//model = glm::scale(model, glm::vec3(5.0f));
 			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
+		glm::mat4 model2;
+		model2 = glm::translate(model2, cubePositions[10]);
+		//model2 = glm::scale(model2, glm::vec3(30.0f, 1.0f, 30.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glBindVertexArray(0);
 
