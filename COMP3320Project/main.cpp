8f25275b8c1b0c2a5fd432aaca6a6ea231cc02b8
jackratcliffe <jack-rat@hotmail.com>
2017-09-15 16:34:06 +1000

    incorporated seb's changes, dependencies still don't work

--- COMP3320Project/main.cpp
+++ COMP3320Project/main.cpp
@@ -6,9 +6,9 @@
 
 #include <SOIL2/SOIL2.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 #include <assimp/Importer.hpp>
 
@@ -20,9 +20,9 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
-void KeyCallback(GLFWwindow *window, int key, int scancode, int action,int mode);
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode);
 void ScrollCallback(GLFWwindow *window, double xOffset, double yOffset);
-void MouseCallback(GLFWwindow *window, double xPos,double yPos);
+void MouseCallback(GLFWwindow *window, double xPos, double yPos);
 void DoMovement();
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
@@ -47,7 +47,7 @@ int main() {
 
 	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "COMP3320 Project", nullptr, nullptr);
 
-	
+
 	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
 
 	if (nullptr == window) {
@@ -80,76 +80,6 @@ int main() {
 
 	Shader ourShader("shaders/core.vs", "shaders/core.frag");
 
-	GLfloat vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(2.0f, 0.0f, -15.0f),
-		glm::vec3(-1.5f, 0.0f, -2.5f),
-		glm::vec3(-3.8f, 0.0f, -12.3f),
-		glm::vec3(2.4f, 0.0f, -3.5f),
-		glm::vec3(-1.7f, 0.0f, -7.5f),
-		glm::vec3(1.3f, 0.0f, -2.5f),
-		glm::vec3(1.5f, 0.0f, -2.5f),
-		glm::vec3(1.5f, 0.0f, -1.5f),
-		glm::vec3(-1.3f, 0.0f, -1.5f),
-		glm::vec3(0.0f, -1.0f, 0.0f),
-	};
-	//glm::vec3 cubePositions[] = {
-	//	glm::vec3(0.0f, 0.0f, 0.0f),
-	//	glm::vec3(2.0f, 5.0f, -15.0f),
-	//	glm::vec3(-1.5f, -2.2f, -2.5f),
-	//	glm::vec3(-3.8f, -2.0f, -12.3f),
-	//	glm::vec3(2.4f, -0.4f, -3.5f),
-	//	glm::vec3(-1.7f, 3.0f, -7.5f),
-	//	glm::vec3(1.3f, -2.0f, -2.5f),
-	//	glm::vec3(1.5f, 2.0f, -2.5f),
-	//	glm::vec3(1.5f, 0.2f, -1.5f),
-	//	glm::vec3(-1.3f, 1.0f, -1.5f),
-	//};
-	
 	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -157,43 +87,13 @@ int main() {
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//Posotion Attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *) 0);
-	glEnableVertexAttribArray(0);
-	//Texture Coordinate Attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
 
+	Model carriageModel("models/Carriage.obj");
+	Model houseModel("models/House.obj");
+	Model terrainModel("models/hill1.obj");
 
-	GLuint textures[2];
-	glGenTextures(2, textures);
-
-	int width, height;
-	unsigned char* image;
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, textures[0]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	image = SOIL_load_image("images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	SOIL_free_image_data(image);
-	glUniform1i(glGetUniformLocation(ourShader.Program, "texture0"), 0);
-
-	Model ourModel("models/Carriage.obj");
-	Model ourModel2("models/House.obj");
-	Model ourModel3("models/Hill.obj");
 
 	glm::mat4 projection = glm::perspective(camera.getZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 1000.0f);
 
@@ -221,71 +121,40 @@ int main() {
 
 		// Draw the loaded model
 		glm::mat4 model;
-		model = glm::translate(model, glm::vec3(6.0f, -1.31f, -5.0f)); // Translate it down a bit so it's at the center of the scene
+		model = glm::translate(model, glm::vec3(3.0f, -4.5f, -4.0f)); // Translate it down a bit so it's at the center of the scene
 		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	// It's a bit too big for our scene, so scale it down
 		glUniformMatrix4fv(glGetUniformLocation(ourShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		ourModel.Draw(ourShader);
+		carriageModel.Draw(ourShader);
 
 		glm::mat4 model4;
-		model4 = glm::translate(model4, glm::vec3(2.0f, -1.31f, 3.0f)); // Translate it down a bit so it's at the center of the scene
+		model4 = glm::translate(model4, glm::vec3(3.0f, -4.5f, 3.0f)); // Translate it down a bit so it's at the center of the scene
 		model4 = glm::scale(model4, glm::vec3(0.5f, 0.5f, 0.5f));	// It's a bit too big for our scene, so scale it down
 		glUniformMatrix4fv(glGetUniformLocation(ourShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model4));
-		ourModel.Draw(ourShader);
+		carriageModel.Draw(ourShader);
 
 		glm::mat4 model2;
-		model2 = glm::translate(model2, glm::vec3(-6.0f, -0.4f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+		model2 = glm::translate(model2, glm::vec3(-6.0f, -3.6f, -5.0f)); // Translate it down a bit so it's at the center of the scene
 		model2 = glm::scale(model2, glm::vec3(0.35f, 0.35f, 0.35f));	// It's a bit too big for our scene, so scale it down
 		glUniformMatrix4fv(glGetUniformLocation(ourShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
-		ourModel2.Draw(ourShader);
+		houseModel.Draw(ourShader);
 
 		glm::mat4 model5;
 		model5 = glm::translate(model5, glm::vec3(-6.0f, -3.05f, 0.0f)); // Translate it down a bit so it's at the center of the scene
 		model5 = glm::scale(model5, glm::vec3(0.35f, 0.35f, 0.35f));	// It's a bit too big for our scene, so scale it down
 		model5 = glm::rotate(model5, 180.0f, glm::vec3(0.0f, 1.0f, 0.0f));	// It's a bit too big for our scene, so scale it down
 		glUniformMatrix4fv(glGetUniformLocation(ourShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model5));
-		ourModel3.Draw(ourShader);
+		terrainModel.Draw(ourShader);
 
-
-
-
-		//glActiveTexture(GL_TEXTURE1);
-		//glBindTexture(GL_TEXTURE_2D, textures[1]);
-		//glUniform1i(glGetUniformLocation(ourShader.Program, "texture1"), 1);
-
-		//ourShader.Use();
-
-		//glm::mat4 projection = glm::perspective(camera.getZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 1000.0f);
-
-		//glm::mat4 model;
-		//glm::mat4 view;
 		view = camera.GetViewMatrix();
 
-		GLint modelLoc = glGetUniformLocation(ourShader.Program,"model");
-		GLint viewLoc = glGetUniformLocation(ourShader.Program,"view");
-		GLint projLoc = glGetUniformLocation(ourShader.Program,"projection");
+		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
 
-		glUniformMatrix4fv(viewLoc,1,GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projLoc,1,GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
-		
-		for (GLuint i = 0; i < 10; i++) {
-			glm::mat4 model;
-			model = glm::translate(model, cubePositions[i]);
-			GLfloat angle = 20.0f * i;
-			//model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
-			//model = glm::scale(model, glm::vec3(5.0f));
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-		glm::mat4 model3;
-		model3 = glm::translate(model3, cubePositions[10]);
-		//model2 = glm::scale(model2, glm::vec3(30.0f, 1.0f, 30.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model3));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		glBindVertexArray(0);
 
 		//DRAW OPENGL
 		glfwSwapBuffers(window);
