7ee870bc5bbd68c58da06d7245ad8b57a07c257b
sebbeth <seb.brown95@gmail.com>
2017-09-29 16:53:19 +1000

    asdfg

--- GLFW_EXAMPLE/LevelData.h
+++ GLFW_EXAMPLE/LevelData.h
@@ -32,20 +32,20 @@ public:
 	LevelData() {
 
 		//Ground object
-	
+	/*
 		objects[0].positioning = glm::translate(objects[0].positioning, glm::vec3(0.0f, 0.0f, 0.0f));
 		float scale = 4.0f;
 		objects[0].positioning = glm::scale(objects[0].positioning, glm::vec3(scale, scale, scale));
 		//objects[0].positioning = glm::rotate(objects[0].positioning, 180.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 		objects[0].model.load("models/mountian3.obj");
-		
+	*/	
 
 		// HOUSE
 		objects[3].positioning = glm::translate(objects[3].positioning, glm::vec3(-6.0f, 30.6f, -5.0f));
 		objects[3].positioning = glm::scale(objects[3].positioning, glm::vec3(0.35f, 0.35f, 0.35f));
 		objects[3].model.load("models/House.obj");
 
-
+/*
 		// Carriage 1
 		objects[1].positioning = glm::translate(objects[1].positioning, glm::vec3(3.0f, 30.5f, -4.0f));
 		objects[1].positioning = glm::scale(objects[1].positioning, glm::vec3(0.5f, 0.5f, 0.5f));
@@ -55,7 +55,7 @@ public:
 		objects[2].positioning = glm::translate(objects[2].positioning, glm::vec3(3.0f, 30.5f, 3.0f));
 		objects[2].positioning = glm::scale(objects[2].positioning, glm::vec3(0.5f, 0.5f, 0.5f));
 		objects[2].model.load("models/Carriage.obj");
-
+		*/
 		
 
 	}
